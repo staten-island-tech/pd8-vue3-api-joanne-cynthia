84595576bd33bb69c4af58fe2774225c324ee440
joannec34 <joannechen9058@gmail.com>
2023-04-19 14:01:55 -0400

    adsfgh

---
+++ vue/src/appVue.vue
@@ -0,0 +1,18 @@
+<template>
+  <header>
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+    </nav>
+  </header>
+
+  <RouterView />
+
+  <homeView />
+</template>
+
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<style scoped></style>
