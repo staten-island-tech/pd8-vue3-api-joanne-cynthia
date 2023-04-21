69533af6adf4587bc44b4f0beeaacc5ae7a79460
joannec34 <joannechen9058@gmail.com>
2023-04-21 01:14:21 -0400

    zzz

--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -2,4 +2,15 @@
   <h1>hello</h1>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref, onMounted } from 'vue'
+const complaint = ref('')
+async function getData() {
+  let res = await fetch('https://data.cityofnewyork.us/resource/qgea-i56i.json')
+  let data = await res.json()
+  console.log(data)
+}
+onMounted(() => {
+  getData()
+})
+</script>
