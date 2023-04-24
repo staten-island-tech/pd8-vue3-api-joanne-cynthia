1e2216c3dbe33aa501f3558902b7dc5a4fb3f055
joannec34 <joannechen9058@gmail.com>
2023-04-24 13:24:04 -0400

    what

--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -1,26 +1,21 @@
 <template>
-  <h1>home</h1>
-  <div class="container">
-    <complaintCard
-      v-for="complaint in complaint"
-      :key="complaint.sample_number"
-      :complaint="complaint"
-    />
-  </div>
+  <h1>homeView</h1>
 </template>
 
-<script setup>
-import { ref, onMounted } from 'vue'
-import complaintCard from '../components/icons/complaintCard.vue'
-
-const complaint = ref('')
-async function getData() {
-  let res = await fetch('https://data.cityofnewyork.us/resource/bkwf-xfky.json')
-  let data = await res.json()
-  complaint.value = data.results
-  console.log(data)
+<script>
+export default {
+  name: 'homeView'
 }
-onMounted(() => {
+/* import { ref, Created } from 'vue'
+const data = ref('')
+async function getData() {
+  let res = await fetch(
+    'https://data.cityofnewyork.us/Public-Safety/NYPD-Complaint-Data-Historic/qgea-i56i'
+  )
+  let data = await res.json()
+  data.value = data.results
+}
+Created(() => {
   getData()
-})
+}) */
 </script>
