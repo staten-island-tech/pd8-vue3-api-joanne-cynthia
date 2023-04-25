11c9c9d93f3b55fd655d74c09ea4bec6d63eac73
joannec34 <joannechen9058@gmail.com>
2023-04-25 13:50:09 -0400

    api

--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -2,13 +2,14 @@
   <h1>hello</h1>
 </template>
 
-<script>
+<script setup>
 import { ref, onMounted } from 'vue'
 const data = ref('')
 async function getData() {
-  let res = await fetch('https://data.cityofnewyork.us/resource/qgea-i56i.json')
+  let res = await fetch('https://data.cityofnewyork.us/resource/fuhs-xmg2.json')
   let data = await res.json()
   data.value = data.results
+  console.log(data)
 }
 onMounted(() => {
   getData()
