e641e76dd7faa63c2f489733a08ac453e433ca94
pinrongc <pinrongc156@gmail.com>
2023-05-01 21:57:45 -0400

    .

--- vue/src/views/PieView.Vue
+++ vue/src/views/PieView.Vue
@@ -82,7 +82,7 @@ export default {
             datasets: [
               {
                 data: filteredData,
-                backgroundColor: ['#B6E2A1', '#FFFBC1', '#FEBE8C', '#F7A4A4']
+                backgroundColor: ['#02d46e', '#fde600', '#ff8440', '#fe6f5e']
               }
             ]
           }
